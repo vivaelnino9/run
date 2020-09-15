5e2a2231cdc178aeac49812cdfd9ed684acca3f7
Lucas McBride <mcbride.lucas9@gmail.com>
2020-09-15 12:13:50 -0400

    switch logic to using curses wrapper

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
 import curses
-
 from game.game import Game
 from ui.ui import Ui
 
-sc = curses.initscr()
 
-uiState = -1
-uiError = ''  # might use later
+def main(stdscr):
+    ui = Ui(-1, '')
+    ui_state = ui.state
 
-HEIGHT, WIDTH = 30, 100
-
-while True:
-    ui = Ui(uiState, uiError)
-    uiState = ui.state
-
-    if uiState == 0:
-        game = Game(HEIGHT, WIDTH)
-        game.connect()
+    if ui_state == 0:
+        game = Game()
         game.start()
-    elif uiState == 2:
-        # Controls
-        pass
-    else:
-        # Exit
-        pass
-    break
 
 
-sc.refresh()
-curses.endwin()
+curses.wrapper(main)
+
